b27efb4937b626b98a6694689eec25abbf7dd0be
Danilo Pereira <danilopx1@gmail.com>
2020-02-20 10:46:44 -0300

    Refatorando as actions

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 
 import { formatPrice } from '../../util/format';
 import { ProductList } from './styles';
 import api from '../../services/api';
+
+import * as CartActions from '../../store/modules/cart/actions';
 
 class Home extends Component {
   constructor(props) {
@@ -26,11 +29,8 @@ class Home extends Component {
   }
 
   handleAddProduct = product => {
-    const { dispatch } = this.props;
-    dispatch({
-      type: 'ADD_TO_CART',
-      product,
-    });
+    const { addToCart } = this.props;
+    addToCart(product);
   };
 
   render() {
@@ -58,4 +58,8 @@ class Home extends Component {
     );
   }
 }
-export default connect()(Home);
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CartActions, dispatch);
+
+export default connect(null, mapDispatchToProps)(Home);
