6d8ca2af2c468fec1eb6034d1af0c37ac5e96a55
Danilo Pereira <danilopx1@gmail.com>
2020-02-20 10:21:37 -0300

    Produto duplicado 2

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,13 +1,24 @@
+import produce from 'immer';
+
 export default function cart(state = [], action) {
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [
-        ...state,
-        {
-          ...action.product,
-          amount: 1,
-        },
-      ];
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.product.id);
+        // procura por um produto que ja esta no nosso carrinho draft.findIndex(p => p.id
+        // E se existe algum produto que o id é igual a esse que estamos recebendo da action -->  === action.product.id
+
+        if (productIndex >= 0) {
+          // se existir adiciona +1 no amount
+          draft[productIndex].amount += 1;
+        } else {
+          // se não, cria novo item no carrinho e adiciona 1 no amount
+          draft.push({
+            ...action.product,
+            amount: 1,
+          });
+        }
+      });
     default:
       return state;
   }
