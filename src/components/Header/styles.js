b824e3b338e3495418a34c5193dc6e05b8c8888f
Danilo Pereira <danilopx1@gmail.com>
2020-02-19 21:57:16 -0300

    Home/Cart estilizado

---
+++ src/components/Header/styles.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const Container = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 25px 0;
+`;
+
+export const Cart = styled(Link)`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  transition: opacity 0.2s; /** sombreamento lento no Cart */
+
+  &:hover {
+    /** efeito de sombreamento */
+    opacity: 0.7;
+  }
+
+  div {
+    text-align: right;
+    margin-right: 10px;
+
+    strong {
+      display: block;
+      color: #fff;
+    }
+
+    span {
+      font-size: 12px;
+      color: #999;
+    }
+  }
+`;
