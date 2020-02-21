a3e5d4556d36b0afab5161c98ca1e4f695ddfbb9
Danilo Pereira <danilopx1@gmail.com>
2020-02-21 17:22:38 -0300

    Separando actions

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,18 +1,41 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 // metodo call responsavel por chamar metodos assincronos de promises
+// metodo select responsavel por fazer buscas dentro do estado
 // metodo put dispara a action
 // takelatest se o usuario fizer varias chamadas ao mesmo tempo,
 // Saga vai descartar apartir da segunda, cadastrando apenas a primeira no carrinho
 import api from '../../../services/api';
-import { addToCartSuccess } from './actions';
+import { addToCartSuccess, updateAmount } from './actions';
+
+import { formatPrice } from '../../../util/format';
 
 function* addToCart({ id }) {
   // (function*) generator como se fosse um async (babel converte async para generator)
-  const response = yield call(api.get, `/products/${id}`);
 
-  yield put(addToCartSuccess(response.data));
+  const productExists = yield select(state =>
+    state.cart.find(p => p.id === id),
+  );
+
+  if (productExists) {
+    const amount = productExists.amount + 1;
+
+    yield put(updateAmount(id, amount));
+  } else {
+    const response = yield call(api.get, `/products/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.price),
+    };
+    yield put(addToCartSuccess(data));
+  }
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
 // primeiro parametro takelatest é qual acao quer disparar
 // segundo parametro takelatest é quao function sera chamada apos a execução da ação
+
+// const productIndex = draft.findIndex(p => p.id === action.product.id);
+// procura por um produto que ja esta no nosso carrinho draft.findIndex(p => p.id
+// E se existe algum produto que o id é igual a esse que estamos recebendo da action -->  === action.product.id
